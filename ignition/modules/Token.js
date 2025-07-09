112fe70bc79f295d30fda82cd0e716f6cc4c1afa
mighty1 <ihanashi@gmail.com>
2025-07-09 20:32:44 +0900

    Smoke Test

---
+++ ignition/modules/Token.js
@@ -0,0 +1,7 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+module.exports = buildModule("TokenModule", (m) => {
+  const token = m.contract("Token", [], {});
+
+  return { token };
+});
