d4702991f224c30021cdd5b17aa8617d7e09aecd
mighty1 <ihanashi@gmail.com>
2025-07-16 20:57:02 +0900

    5. Transfer Tokens

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,6 +9,9 @@ contract Token {
 
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
     constructor(
         string memory _name, 
         string memory _symbol, 
@@ -18,5 +21,17 @@ contract Token {
         symbol = _symbol;
         totalSupply = _totalSupply * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Token: Insufficient Funds");
+        require(_to != address(0), "Token: Recipient is address 0");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
