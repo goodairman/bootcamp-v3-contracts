b984bf87b8f06e9473b171738e8cccaa81aa2c19
mighty1 <ihanashi@gmail.com>
2025-07-31 20:44:09 +0900

    9. Deposit Tokens

--- test/Exchange.js
+++ test/Exchange.js
@@ -2,7 +2,7 @@ const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helper
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
-const { deployExchangeFixture } = require("./helpers/ExchangeFixtures")
+const { deployExchangeFixture, depositExchangeFixture } = require("./helpers/ExchangeFixtures")
 
 const tokens = (n) => {
     return ethers.parseUnits(n.toString(), 18)
@@ -19,5 +19,38 @@ describe("Exchange", () => {
             const { exchange } = await loadFixture(deployExchangeFixture)
             expect(await exchange.feePercent()).to.equal(10)
         })
+    })
+
+    describe("Depositing Tokens", () => {
+        const AMOUNT = tokens("100")
+
+        describe("Success", () => {
+            it("tracks the token deposit", async () => {
+                const { tokens: { token0 }, exchange, accounts } = await loadFixture(depositExchangeFixture)
+                expect(await token0.balanceOf(await exchange.getAddress())).to.equal(AMOUNT)
+                expect(await exchange.totalBalanceOf(await exchange.getAddress(), accounts.user1.address))
+            })
+
+            it("emits a TokensDeposited event", async () => {
+                const { tokens: { token0 }, exchange, accounts, transaction } = await loadFixture(depositExchangeFixture)
+                await expect(transaction).to.emit(exchange, "TokensDeposited")
+                    .withArgs(
+                        await token0.getAddress(),
+                        accounts.user1.address,
+                        AMOUNT,
+                        AMOUNT
+                    )
+            })
+
+        })
+
+        describe("Failure", () => {
+            it("fails when no tokens are approved", async () => {
+                const { tokens: { token0 }, exchange, accounts } = await loadFixture(deployExchangeFixture)
+                await expect(exchange.connect(accounts.user1).depositToken(await token0.getAddress(), AMOUNT)).to.be.reverted
+            })
+
+        })
+
     })
 })
--- test/helpers/ExchangeFixtures.js
+++ test/helpers/ExchangeFixtures.js
@@ -1,20 +1,55 @@
 const { ethers } = require("hardhat")
 
-
 async function deployExchangeFixture() {
     const Exchange = await ethers.getContractFactory("Exchange")
+    const Token = await ethers.getContractFactory("Token")
+
+    const token0 = await Token.deploy("Dapp University", "DAPP", "1000000")
+    const token1 = await Token.deploy("Mock Dai", "mDAI", "1000000")
 
     const accounts = await ethers.getSigners()
     const deployer = accounts[0]
     const feeAccount = accounts[1]
+    const user1 = accounts[2]
+    const user2 = accounts[3]
+
+    const AMOUNT = ethers.parseUnits("100", 18)
+
+    // Transfer 100 token0 to user 1
+    await (await token0.connect(deployer).transfer(user1.address, AMOUNT)).wait()
+
+    // Transfer 100 token1 to user 2
+    await (await token1.connect(deployer).transfer(user2.address, AMOUNT)).wait()
 
     const FEE_PERCENT = 10
-
     const exchange = await Exchange.deploy(feeAccount, FEE_PERCENT)
 
-    return { exchange, accounts: { deployer, feeAccount } }
+    return { tokens: { token0, token1 }, exchange, accounts: { deployer, feeAccount, user1, user2 } }
+}
+
+async function depositExchangeFixture() {
+    const { tokens, exchange, accounts } = await deployExchangeFixture()
+
+    const AMOUNT = ethers.parseUnits("100", 18)
+
+    // Approve token0 for user1
+    await (await tokens.token0.connect(accounts.user1).approve(await exchange.getAddress(), AMOUNT)).wait()
+
+    // Deposit token0 for user1
+    // We need the transaction variable to test in our unit test
+    const transaction = await exchange.connect(accounts.user1).depositToken(await tokens.token0.getAddress(), AMOUNT)
+    await transaction.wait()
+
+    // Approve and deposit token 1 for user2
+    // We do not need the trnsaction variable for our tests, so we can
+    // just do the transaction and await it.
+    await (await tokens.token1.connect(accounts.user2).approve(await exchange.getAddress(), AMOUNT)).wait()
+    await (await exchange.connect(accounts.user2).depositToken(await tokens.token1.getAddress(), AMOUNT)).wait()
+
+    return { tokens, exchange, accounts, transaction }
 }
 
 module.exports = {
-    deployExchangeFixture
+    deployExchangeFixture,
+    depositExchangeFixture
 }
