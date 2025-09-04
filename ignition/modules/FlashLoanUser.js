4edc72010c765fbe076340f90b33482bc80315fe
mighty1 <ihanashi@gmail.com>
2025-09-04 15:36:23 +0900

    16. Seed exchange

---
+++ ignition/modules/FlashLoanUser.js
@@ -0,0 +1,16 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+module.exports = buildModule("UserModule", (m) => {
+  // Remember #0 is deployer and #1 is the fee account
+  const USER = m.getAccount(2)
+
+  const EXCHANGE_ADDRESS = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+
+  const flashLoanUser = m.contract(
+    "FlashLoanUser",
+    [EXCHANGE_ADDRESS],
+    { from: USER }
+  )
+
+  return { flashLoanUser }
+});
--- ignition/modules/Token.js
+++ ignition/modules/Token.js
@@ -1,7 +1,26 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("TokenModule", (m) => {
-  const token = m.contract("Token", [], {});
+  const TOTAL_SUPPLY = 1000000;
+  const DEPLOYER = m.getAccount(0);
 
-  return { token };
+  const DAPP = m.contract(
+    "Token",
+    ["Dapp University", "DAPPU", TOTAL_SUPPLY],
+    { from: DEPLOYER, id: "DAPP" }
+  )
+
+  const mUSDC = m.contract(
+    "Token",
+    ["Mock USDC", "mUSDC", TOTAL_SUPPLY],
+    { from: DEPLOYER, id: "mUSDC" }
+  )
+
+  const mLINK = m.contract(
+    "Token",
+    ["Mock LINK", "mLINK", TOTAL_SUPPLY],
+    { from: DEPLOYER, id: "mLINK" }
+  )
+
+  return { DAPP, mUSDC, mLINK };
 });
